e23b760459e652798f1dc0100e6f832ed3f5f600
Earthlingz <Earthlingz@7321b99d-e950-40b2-8679-7769d4a61ae3>
2012-02-12 22:34:43 +0000

    2011-07-13 kRO Patch lua and lub
    git-svn-id: http://subversion.assembla.com/svn/ClientSide@140 7321b99d-e950-40b2-8679-7769d4a61ae3

--- Lua_Project/lua files/datainfo/npclocationradius.lua
+++ Lua_Project/lua files/datainfo/npclocationradius.lua
@@ -1,7 +1,6 @@
 NpcLocationRadius = {
 
-	[jobtbl.JT_E_VADON_X] = 4,
-	[jobtbl.JT_E_RANDGRIS] = 3,
-	[jobtbl.JT_EVENT_BAPHO] = 2,
-	[jobtbl.JT_E_GIANT_OCTOPUS] = 1
+	[jobtbl.JT_GIOIA] = 1,
+	[jobtbl.JT_DAEHYON] = 1,
+	[jobtbl.JT_KADES] = 1,
 }
