09f15e4cf10249dc80e2d699eb8a018fd36d5d39
Earthlingz <Earthlingz@7321b99d-e950-40b2-8679-7769d4a61ae3>
2012-02-21 20:13:08 +0000

    Fixed a mistake in spriterobename.lua
    git-svn-id: http://subversion.assembla.com/svn/ClientSide@154 7321b99d-e950-40b2-8679-7769d4a61ae3

--- Lua_Project/lua files/datainfo/spriterobename_f.lua
+++ Lua_Project/lua files/datainfo/spriterobename_f.lua
@@ -12,15 +12,15 @@ end
 
 ReqRobSprName_V2 = function(index, in_bResEnglishName)
 	if in_bResEnglishName == true then
-	if RobeNameTable_Eng[index] == nil then
-		return ""
-	end
+		if RobeNameTable_Eng[index] == nil then
+			return ""
+		end
 	local NameMsg = RobeNameTable_Eng[index]
-	if NameMsg ~= nil then
-		return NameMsg
-	else
-		return ""
-	end
+		if NameMsg ~= nil then
+			return NameMsg
+		else
+			return ""
+		end
 	else
 		ReqRobSprName(index)
 	end
